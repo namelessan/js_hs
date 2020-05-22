e5095fd0a1fa72e0afb57d55e875c0a1a599b4dd
namelessan <phamhaian.qb@gmail.com>
2020-05-22 11:50:44 +0700

    module-3: finish guessSecret by class solution

--- module 3/guessSecret/guessSecret.js
+++ module 3/guessSecret/guessSecret.js
@@ -1,21 +1,31 @@
-// class GuessSecret {
-//   constructor(value, times = 3) {
-//     this.value = value;
-//     this.times = times;
-//     this.timeLeft = times;
-//   }
+// class solutions
+class GuessSecret {
+  constructor(value, times = 3) {
+    let timeLeft = times;
+    this.try = function (guess) {
+      if (timeLeft === 0) {
+        console.log(
+          'Sorry, no guesses left. The secret can never be revealed.'
+        );
+        return;
+      }
 
-//   try(guess) {
-//     if (this.timeLeft === 0)
-//       console.log('Sorry, no guesses left. The secret can never be revealed.');
+      if (timeLeft > 0 && value !== guess) {
+        timeLeft--;
+        console.log(
+          `You guess wrong.${timeLeft} left${timeLeft > 1 ? 's' : ''}`
+        );
+        return;
+      }
 
-//     if (this.timeLeft > 0 && this.value !== guess)
-//       console.log(
-//         `You guess wrong.${this.timeLeft} left${this.timeLeft > 1 ? 's' : ''}`
-//       );
-//   }
-// }
+      console.log('The secret has been revealed!');
+      this.secret = value;
+      delete this.try;
+    };
+  }
+}
 
+// function solution
 const GuessSecret = function guessSecret(value, times = 3) {
   let timeLeft = times;
   this.try = function (guess) {
@@ -39,26 +49,26 @@ const GuessSecret = function guessSecret(value, times = 3) {
   };
 };
 
-const home = new GuessSecret('home');
+// const home = new GuessSecret('home');
 
-console.log(Object.getOwnPropertyNames(home)); // $> [ 'try' ]
+// console.log(Object.getOwnPropertyNames(home)); // $> [ 'try' ]
 
-home.try('cat'); // $> You guessed wrong. 2 tries left.
+// home.try('cat'); // $> You guessed wrong. 2 tries left.
 
-home.try('spaceship'); // $> You guessed wrong. 1 try left.
+// home.try('spaceship'); // $> You guessed wrong. 1 try left.
 
-home.try('potato'); // $> You guessed wrong. 0 tries left.
+// home.try('potato'); // $> You guessed wrong. 0 tries left.
 
-home.try('home'); // $> Sorry, no guesses left. The secret can never be revealed.
+// home.try('home'); // $> Sorry, no guesses left. The secret can never be revealed.
 
-console.log(Object.getOwnPropertyNames(home)); // $> [ 'try' ]
+// console.log(Object.getOwnPropertyNames(home)); // $> [ 'try' ]
 
-const nb = new GuessSecret(25, 10);
+// const nb = new GuessSecret(25, 10);
 
-console.log(Object.getOwnPropertyNames(nb)); // $> [ 'try' ]
+// console.log(Object.getOwnPropertyNames(nb)); // $> [ 'try' ]
 
-nb.try('25'); // $> You guessed wrong. 9 tries left.
+// nb.try('25'); // $> You guessed wrong. 9 tries left.
 
-nb.try(25); // $> The secret has been revealed!
+// nb.try(25); // $> The secret has been revealed!
 
-console.log(Object.getOwnPropertyNames(nb)); // $> [ 'secret' ]
+// console.log(Object.getOwnPropertyNames(nb)); // $> [ 'secret' ]
